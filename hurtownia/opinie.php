74aeff0e89e14b05501d991558aa188794844009
losmok <113143965+losmok@users.noreply.github.com>
2023-10-16 13:48:30 +0200

    Add files via upload

---
+++ hurtownia/opinie.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Opinie klientów</title>
+    <link rel="stylesheet" href="styl3.css">
+</head>
+<body>
+    <div class="baner">
+        <h1>Hurtownia spożywcza</h1>
+    </div>
+    <div class="main">
+        <h2>Opinie naszych klientów</h2>
+        <!-- Script -->
+    </div>
+    <div class="footer-1">
+        <h3>Współpracują z nami</h3>
+        <a href="http://sklep.pl/">Sklep 1</a>
+    </div>
+    <div class="footer-2">
+        <h3>Nasi top klienci</h3>
+        <ul>
+            <!-- Script 2 -->
+        </ul>
+    </div>
+    <div class="footer-3">
+        <h3>Skontaktuj się</h3>
+        <p>telefon: 111222333</p>
+    </div>
+    <div class="footer-4">
+        <h3>Autor: Paweł Lewandowski</h3>
+    </div>
+</body>
+</html>
